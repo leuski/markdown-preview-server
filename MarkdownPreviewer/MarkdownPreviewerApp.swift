2b0473a2901e64b41ba5a23f8488850222f0aebb
Anton Leuski <anton@leuski.net>
2026-04-25 21:53:28 -0700

    Initial Commit

---
+++ MarkdownPreviewer/MarkdownPreviewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarkdownPreviewerApp.swift
+//  MarkdownPreviewer
+//
+//  Created by Anton Leuski on 4/25/26.
+//
+
+import SwiftUI
+
+@main
+struct MarkdownPreviewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MarkdownPreviewerTests/MarkdownPreviewerTests.swift
@@ -0,0 +1,19 @@
+//
+//  MarkdownPreviewerTests.swift
+//  MarkdownPreviewerTests
+//
+//  Created by Anton Leuski on 4/25/26.
+//
+
+import Testing
+@testable import MarkdownPreviewer
+
+struct MarkdownPreviewerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
