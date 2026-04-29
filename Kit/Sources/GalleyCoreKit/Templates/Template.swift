c40138cfd7ca24d79c859846ea28091fefb36c22
Anton Leuski <anton@leuski.net>
2026-04-29 16:53:57 -0700

    cleaning up

--- Kit/Sources/GalleyCoreKit/Templates/Template.swift
+++ Kit/Sources/GalleyCoreKit/Templates/Template.swift
@@ -3,7 +3,6 @@ import Foundation
 public protocol Template: Identifiable, Sendable {
   var id: String { get }
   var name: String { get }
-  var isBuiltIn: Bool { get }
   func loadHTML() throws -> String
   func rewriteAssets(in html: String, origin: URL) -> String
   func resolveAsset(file: String) -> URL?
--- Kit/Sources/GalleyCoreKit/Templates/UserTemplate.swift
+++ Kit/Sources/GalleyCoreKit/Templates/UserTemplate.swift
@@ -5,7 +5,6 @@ public struct UserTemplate: Template {
   public let name: String
   public let directoryURL: URL
   public let htmlURL: URL
-  public var isBuiltIn: Bool { false }
 
   public init(
     id: String, name: String, directoryURL: URL, htmlURL: URL
